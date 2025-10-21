3f95deea37c6972257ea1ae4400cc38e8983ba43
LopezRussiAlvaroFelipe <aflopez67@ucatolica.edu.co>
2025-10-20 19:22:31 -0500

    "deploy"

---
+++ proyecto/.vercel/output/_functions/entry.mjs
@@ -0,0 +1,32 @@
+import { renderers } from './renderers.mjs';
+import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_DMwonuQA.mjs';
+import { manifest } from './manifest_Dp6NdgVZ.mjs';
+
+const serverIslandMap = new Map();;
+
+const _page0 = () => import('./pages/_image.astro.mjs');
+const _page1 = () => import('./pages/servicios/_slug_.astro.mjs');
+const _page2 = () => import('./pages/index.astro.mjs');
+const pageMap = new Map([
+    ["node_modules/astro/dist/assets/endpoint/generic.js", _page0],
+    ["src/pages/servicios/[slug].astro", _page1],
+    ["src/pages/index.astro", _page2]
+]);
+
+const _manifest = Object.assign(manifest, {
+    pageMap,
+    serverIslandMap,
+    renderers,
+    actions: () => import('./noop-entrypoint.mjs'),
+    middleware: () => import('./_noop-middleware.mjs')
+});
+const _args = {
+    "middlewareSecret": "050e5a56-5115-4876-b71e-2562c9e67618",
+    "skewProtection": false
+};
+const _exports = createExports(_manifest, _args);
+const __astrojsSsrVirtualEntry = _exports.default;
+const _start = 'start';
+if (Object.prototype.hasOwnProperty.call(serverEntrypointModule, _start)) ;
+
+export { __astrojsSsrVirtualEntry as default, pageMap };
