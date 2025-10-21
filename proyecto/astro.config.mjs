ba61105ab1ba8395f0c33f5917d76f9ba31161e3
LopezRussiAlvaroFelipe <aflopez67@ucatolica.edu.co>
2025-10-20 19:19:50 -0500

    "vercel installing"

--- proyecto/astro.config.mjs
+++ proyecto/astro.config.mjs
@@ -1,13 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import vercel from '@astrojs/vercel/serverless'
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'static',
+  output: 'server',
+  adapter: vercel({}),
   site: 'https://dra.susanagrajales.com',
   vite: {
     plugins: [tailwindcss()],
